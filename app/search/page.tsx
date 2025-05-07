882c8f4a969ca16be5beba204be2b11d7f9080a9
samscript18 <adeolasam20@gmail.com>
2025-05-07 15:10:10 +0100

    ...

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -29,7 +29,7 @@ export default function HomePage() {
 	);
 }
 
-export function DocumentsSkeleton() {
+function DocumentsSkeleton() {
 	return (
 		<div className="container py-8">
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
--- components/ui/admin/documents/index.tsx
+++ components/ui/admin/documents/index.tsx
@@ -2,7 +2,7 @@
 import SearchSection from './search-section';
 import { Suspense } from 'react';
 import DocumentsSection from './document-section';
-import { DocumentsSkeleton } from '@/app/search/page';
+import { Skeleton } from '../../skeleton';
 
 const DocumentPage = () => {
 	return (
@@ -14,4 +14,23 @@ const DocumentPage = () => {
 		</section>
 	);
 };
+
+function DocumentsSkeleton() {
+	return (
+		<div className="container py-8">
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+				{Array(6)
+					.fill(0)
+					.map((_, i) => (
+						<div key={i} className="border rounded-lg p-4 space-y-3">
+							<Skeleton className="h-6 w-3/4" />
+							<Skeleton className="h-4 w-1/2" />
+							<Skeleton className="h-4 w-1/4" />
+							<Skeleton className="h-10 w-full" />
+						</div>
+					))}
+			</div>
+		</div>
+	);
+}
 export default DocumentPage;
